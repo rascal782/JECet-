c41e2a27fba95c45d1a9deb4eb35353fc1b2ae04
rascal782 <33050422+rascal782@users.noreply.github.com>
2019-06-25 16:15:59 +0900

    去年のコードを追加

--- EtRobo2019/app.cpp
+++ EtRobo2019/app.cpp
@@ -6,107 +6,57 @@
  *  Copyright (c) 2015 Embedded Technology Software Design Robot Contest
  *****************************************************************************/
 
+#include "ev3api.h"
 #include "app.h"
-#include "LineTracerWithStarter.h"
-
 // デストラクタ問題の回避
 // https://github.com/ETrobocon/etroboEV3/wiki/problem_and_coping
-void *__dso_handle=0;
+#include "runner/Driver.h"
 
-// using宣言
-using ev3api::ColorSensor;
-using ev3api::GyroSensor;
-using ev3api::TouchSensor;
-using ev3api::Motor;
+#define DEBUG
 
-// Device objects
-// オブジェクトを静的に確保する
-ColorSensor gColorSensor(PORT_3);
-GyroSensor  gGyroSensor(PORT_4);
-TouchSensor gTouchSensor(PORT_1);
-Motor       gLeftWheel(PORT_C);
-Motor       gRightWheel(PORT_B);
+#ifdef DEBUG
+#define _debug(x) (x)
+#else
+#define _debug(x)
+#endif
 
-// オブジェクトの定義
-static LineMonitor     *gLineMonitor;
-static Balancer        *gBalancer;
-static BalancingWalker *gBalancingWalker;
-static LineTracer      *gLineTracer;
-static Starter         *gStarter;
-static LineTracerWithStarter *gLineTracerWithStarter;
-
-/**
- * EV3システム生成
- */
-static void user_system_create() {
-    // [TODO] タッチセンサの初期化に2msのdelayがあるため、ここで待つ
-    tslp_tsk(2);
-
-    // オブジェクトの作成
-    gBalancer        = new Balancer();
-    gBalancingWalker = new BalancingWalker(gGyroSensor,
-                                           gLeftWheel,
-                                           gRightWheel,
-                                           gBalancer);
-    gLineMonitor     = new LineMonitor(gColorSensor);
-    gStarter         = new Starter(gTouchSensor);
-    gLineTracer      = new LineTracer(gLineMonitor, gBalancingWalker);
-    gLineTracerWithStarter = new LineTracerWithStarter(gLineTracer, gStarter);
-
-    // 初期化完了通知
-    ev3_led_set_color(LED_ORANGE);
-}
-
-/**
- * EV3システム破棄
- */
-static void user_system_destroy() {
-    gLeftWheel.reset();
-    gRightWheel.reset();
-
-    delete gLineTracerWithStarter;
-    delete gLineTracer;
-    delete gStarter;
-    delete gLineMonitor;
-    delete gBalancingWalker;
-    delete gBalancer;
-}
-
-/**
- * トレース実行タイミング
- */
-void ev3_cyc_tracer(intptr_t exinf) {
-    act_tsk(TRACER_TASK);
-}
+Driver* driver;
 
 /**
  * メインタスク
+ *　@param unused 拡張情報
  */
-void main_task(intptr_t unused) {
-    user_system_create();  // センサやモータの初期化処理
-    syslog
-    // 周期ハンドラ開始
-    ev3_sta_cyc(EV3_CYC_TRACER);
+void mainTask(intptr_t unused){
+    char buf[64];
+    driver = new Driver();
 
-    slp_tsk();  // バックボタンが押されるまで待つ
+    act_tsk(BT_TASK);
 
-    // 周期ハンドラ停止
-    ev3_stp_cyc(EV3_CYC_TRACER);
+    driver->start();
 
-    user_system_destroy();  // 終了処理
+    ER er = ev3_sta_cyc(CYC_HANDLER);
+    sprintf(buf, "main_task: error_code=%d",MERCD(er))
 
+    slp_tsk();
     ext_tsk();
 }
 
 /**
- * ライントレースタスク
+ * コントロールタスク
+ * @param unused 拡張情報
  */
-void tracer_task(intptr_t exinf) {
-    if (ev3_button_is_pressed(BACK_BUTTON)) {
-        wup_tsk(MAIN_TASK);  // バックボタン押下
-    } else {
-        gLineTracerWithStarter->run();  // 倒立走行
-    }
+void controllerTask(intptr_t unused){
+    driver->exec();
+}
 
-    ext_tsk();
+/**
+ * BTタスク
+ * @param unused 拡張情報
+ * `comment Blutppth通信によるリモートスタート。Tera Termなどのターミナルソフトから、ASCIIコードで1を送信すると、リモートスタートする。
+ */
+
+void btTask(intptr_t unused){
+    while(1){
+        driver->btUpdate();
+    }
 }
